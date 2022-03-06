ca16e49c67506c0fa764ba3c441ef80a5a289388
MerveKaradas <100419329+MerveKaradas@users.noreply.github.com>
2022-03-06 23:43:31 +0300

    Create evcizme.c

---
+++ evcizme.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void ucgen()
+{
+	printf("        /\\ \n");
+	printf("       /  \\ \n");
+	printf("      /    \\ \n");
+	printf("     /      \\ \n");
+	printf("    /        \\ \n");
+	printf("   /          \\ \n");
+	printf("  /            \\ \n");
+	printf(" |--------------| \n");
+	
+}
+
+void dikdortgen()
+{
+	printf(" |--------------|\n");
+	printf(" |              |\n");
+	printf(" |              |\n");
+	printf(" |              |\n");
+	printf(" |              |\n");
+	printf(" |              |\n");
+	printf(" |--------------|\n");
+}
+
+void ikisatirbosluk()
+{
+	printf("\n\n");
+}
+
+int main(int argc, char *argv[]) {
+	
+	ucgen();
+	dikdortgen();
+	ikisatirbosluk();
+	ucgen();
+	dikdortgen();
+	return 0;
+}
